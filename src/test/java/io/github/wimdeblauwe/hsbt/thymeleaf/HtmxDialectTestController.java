152f0577717062109db9e3d74318464eee8f2e92
Wim Deblauwe <wim.deblauwe@gmail.com>
2022-05-04 20:59:30 +0200

    Add unit test for HtmxDialect

---
+++ src/test/java/io/github/wimdeblauwe/hsbt/thymeleaf/HtmxDialectTestController.java
@@ -0,0 +1,20 @@
+package io.github.wimdeblauwe.hsbt.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/htmx-dialect")
+public class HtmxDialectTestController {
+    @GetMapping
+    public String htmxDialect(Model model) {
+        model.addAttribute("trueVariable", true);
+        model.addAttribute("falseVariable", false);
+        model.addAttribute("listVariable", List.of(1, 2, 3));
+        return "htmx-dialect-test";
+    }
+}
