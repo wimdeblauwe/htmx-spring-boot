5a4b0910387582b3525ce0223469f34fae52664e
xhaggi <sascha.woo@gmail.com>
2023-08-28 16:09:34 +0200

    Add support for HX-Location response header in HtmxResponse

--- htmx-spring-boot/src/test/java/io/github/wimdeblauwe/htmx/spring/boot/mvc/HtmxViewHandlerInterceptorTest.java
+++ htmx-spring-boot/src/test/java/io/github/wimdeblauwe/htmx/spring/boot/mvc/HtmxViewHandlerInterceptorTest.java
@@ -19,6 +19,20 @@ public class HtmxViewHandlerInterceptorTest {
     private MockMvc mockMvc;
 
     @Test
+    public void testHxLocationWithContextData() throws Exception {
+        mockMvc.perform(get("/hvhi/hx-location-with-context-data"))
+               .andExpect(status().isOk())
+               .andExpect(header().string("HX-Location", "{\"path\":\"/path\",\"source\":\"source\",\"event\":\"event\",\"handler\":\"handler\",\"target\":\"target\",\"swap\":\"swap\",\"values\":{\"value1\":\"v1\",\"value2\":\"v2\"},\"headers\":{\"header1\":\"v1\",\"header2\":\"v2\"}}"));
+    }
+
+    @Test
+    public void testHxLocationWithoutContextData() throws Exception {
+        mockMvc.perform(get("/hvhi/hx-location-without-context-data"))
+               .andExpect(status().isOk())
+               .andExpect(header().string("HX-Location", "/path"));
+    }
+
+    @Test
     public void testHxReswap() throws Exception {
         mockMvc.perform(get("/hvhi/hx-reswap"))
                .andExpect(status().isOk())
