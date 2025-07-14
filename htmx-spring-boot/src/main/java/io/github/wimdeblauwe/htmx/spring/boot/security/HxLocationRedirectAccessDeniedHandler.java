e73c554cfd9f33df7661f9f1b0071881490b6946
Leonardo Cabrera <lcnicolau@gmx.com>
2025-07-14 12:03:14 -0400

    Remove session usage for error handling

--- htmx-spring-boot/src/main/java/io/github/wimdeblauwe/htmx/spring/boot/security/HxLocationRedirectAccessDeniedHandler.java
+++ htmx-spring-boot/src/main/java/io/github/wimdeblauwe/htmx/spring/boot/security/HxLocationRedirectAccessDeniedHandler.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
@@ -24,29 +23,20 @@ import java.io.IOException;
 public class HxLocationRedirectAccessDeniedHandler implements AccessDeniedHandler {
 
     private final String redirectUrl;
-    private final boolean allowSessionCreation;
     private final RedirectStrategy redirectStrategy;
 
     public HxLocationRedirectAccessDeniedHandler(String redirectUrl) {
-        this(redirectUrl, true);
+        this(redirectUrl, new HxLocationRedirectStrategy(HttpStatus.FORBIDDEN));
     }
 
-    public HxLocationRedirectAccessDeniedHandler(String redirectUrl, boolean allowSessionCreation) {
-        this(redirectUrl, allowSessionCreation, new HxLocationRedirectStrategy(HttpStatus.FORBIDDEN));
-    }
-
-    public HxLocationRedirectAccessDeniedHandler(String redirectUrl, boolean allowSessionCreation, RedirectStrategy redirectStrategy) {
+    public HxLocationRedirectAccessDeniedHandler(String redirectUrl, RedirectStrategy redirectStrategy) {
         this.redirectUrl = redirectUrl;
-        this.allowSessionCreation = allowSessionCreation;
         this.redirectStrategy = redirectStrategy;
     }
 
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        if (request.getSession(false) != null || this.allowSessionCreation) {
-            request.getSession().setAttribute(WebAttributes.ACCESS_DENIED_403, accessDeniedException);
-        }
         redirectStrategy.sendRedirect(request, response, redirectUrl);
     }
 
--- htmx-spring-boot/src/main/java/io/github/wimdeblauwe/htmx/spring/boot/security/HxLocationRedirectAuthenticationEntryPoint.java
+++ htmx-spring-boot/src/main/java/io/github/wimdeblauwe/htmx/spring/boot/security/HxLocationRedirectAuthenticationEntryPoint.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
 
 import java.io.IOException;
 
@@ -24,29 +23,20 @@ import java.io.IOException;
 public class HxLocationRedirectAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final String redirectUrl;
-    private final boolean allowSessionCreation;
     private final RedirectStrategy redirectStrategy;
 
     public HxLocationRedirectAuthenticationEntryPoint(String redirectUrl) {
-        this(redirectUrl, true);
+        this(redirectUrl, new HxLocationRedirectStrategy(HttpStatus.UNAUTHORIZED));
     }
 
-    public HxLocationRedirectAuthenticationEntryPoint(String redirectUrl, boolean allowSessionCreation) {
-        this(redirectUrl, allowSessionCreation, new HxLocationRedirectStrategy(HttpStatus.UNAUTHORIZED));
-    }
-
-    public HxLocationRedirectAuthenticationEntryPoint(String redirectUrl, boolean allowSessionCreation, RedirectStrategy redirectStrategy) {
+    public HxLocationRedirectAuthenticationEntryPoint(String redirectUrl, RedirectStrategy redirectStrategy) {
         this.redirectUrl = redirectUrl;
-        this.allowSessionCreation = allowSessionCreation;
         this.redirectStrategy = redirectStrategy;
     }
 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        if (request.getSession(false) != null || this.allowSessionCreation) {
-            request.getSession().setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, authException);
-        }
         redirectStrategy.sendRedirect(request, response, redirectUrl);
     }
 
